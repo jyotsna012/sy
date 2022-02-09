df63295abdb38c0ffa3131bb96b9c203f6e86f99
jyotsna012 <88685258+jyotsna012@users.noreply.github.com>
2022-02-09 12:45:06 -0800

    Update nodeForTree.cpp

--- nodeForTree.cpp
+++ nodeForTree.cpp
@@ -1,1 +1,44 @@
+#include "nodeForTree.h"
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+nodeForTree::nodeForTree(char* newVal){
+  val = newVal;
+  left = NULL;
+  right = NULL;
+  next = NULL;
+}
+
+nodeForTree* nodeForTree::getLeft(){
+  return left;
+}
+
+nodeForTree* nodeForTree::getRight(){
+  return right;
+}
+
+nodeForTree* nodeForTree::getNext(){
+  return next;
+}
+
+char* nodeForTree::getVal(){
+  return val;
+}
+
+void nodeForTree::setLeft(nodeForTree* newLeft){
+  left = newLeft;
+}
+
+void nodeForTree::setRight(nodeForTree* newRight){
+  right = newRight;
+}
+
+void nodeForTree::setNext(nodeForTree* newNext){
+  next = newNext;
+}
+
+
+
 
