842f222ab0bb320553c2b0ca50351b88e76c5a2c
jyotsna012 <88685258+jyotsna012@users.noreply.github.com>
2022-02-08 21:55:30 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -131,6 +131,9 @@ void infixToPostfix(char equation[80]){
       add(qHead, qTail, addIt);
       printQueue(qHead);
     }
+    if(equation[i] == '+' || '-' || '*' || '^' || '/'){
+      cout<<"op"<<endl;
+    }
   }
 }
 
