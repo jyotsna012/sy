cfc7f57e12b570488fd34a79af36dc12ed3c44e4
jyotsna012 <88685258+jyotsna012@users.noreply.github.com>
2022-02-09 23:15:31 -0800

    Update nodeForTree.h

--- nodeForTree.h
+++ nodeForTree.h
@@ -4,12 +4,14 @@
 using namespace std;
 class nodeForTree{
  public:
-  nodeForTree(char*);
+  nodeForTree();
   void setLeft(nodeForTree*);
   void setRight(nodeForTree*);
   nodeForTree* getLeft();
   nodeForTree* getRight();
   char* getVal();
+  void setVal(char);
+
  private:
   nodeForTree* left;
   nodeForTree* right;
