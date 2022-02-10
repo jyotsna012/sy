e7c47a01756755df7f4cfea46aa7278f39339da3
jyotsna012 <88685258+jyotsna012@users.noreply.github.com>
2022-02-09 22:18:26 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -221,7 +221,7 @@ void infixToPostfix(char equation[80]){
   printQueue(qHead);
   char* toReturn = new char[countQueue(qHead)];
   for(int z = 0; z<countQueue(qHead); z++){
-    char* temp[1] = {z}
+    char* temp[1] = {'j'};
     strcpy(toReturn, z);
   }
   cout << "toR: " << toReturn << endl;
