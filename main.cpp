c88807d52b69a3d97037ef0fb4d37dded594dc66
jyotsna012 <88685258+jyotsna012@users.noreply.github.com>
2022-02-10 12:01:06 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -267,29 +267,51 @@ void treepush(nodeForTree* &head, nodeForTree* toPush){
 }
 
 void toTree(char* queueChar){
+  cout << "hi 1: " << endl;
   nodeForTree* head = NULL;
+  cout << "hi 2: " << endl;
   for(int i = 0; i < strlen(queueChar); i++){
+      cout << "hi 3: " << endl;
     if(isdigit(queueChar[i])){
+        cout << "hi 4: " << endl;
       nodeForTree* addIt = new nodeForTree;
+        cout << "hi 5: " << endl;
       char val = queueChar[i];
+        cout << "hi 6: " << endl;
       addIt -> setVal(val);
+        cout << "hi 7: " << endl;
       nodeForTree* left = NULL;
+        cout << "hi 8: " << endl;
       nodeForTree* right = NULL;
+        cout << "hi 9: " << endl;
       head -> setRight(right);
+        cout << "hi 10: " << endl;
       head -> setLeft(left);
+        cout << "hi 11: " << endl;
       treepush(head, addIt);
+        cout << "hi 12: " << endl;
     }else{
+        cout << "hi 13: " << endl;
        nodeForTree* top = NULL;
+        cout << "hi 14: " << endl;
        nodeForTree* left = treepop(head);
+        cout << "hi 15: " << endl;
        nodeForTree* right = treepop(head);
+        cout << "hi 16: " << endl;
        top -> setRight(right);
+        cout << "hi 17: " << endl;
        top -> setLeft(left);
+        cout << "hi 18: " << endl;
        char val = queueChar[i];
+        cout << "hi 19: " << endl;
        top -> setVal(val);
+        cout << "hi 20: " << endl;
        treepush(head, top);
     }
+      cout << "hi 21: " << endl;
   }
   
+    cout << "hi 22: " << endl;
   cout << countTreeStack(head) << endl;
   
 }
