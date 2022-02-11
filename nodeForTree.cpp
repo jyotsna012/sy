08bb639fde404c43beda37c28367dbb2ba31d672
jyotsna012 <88685258+jyotsna012@users.noreply.github.com>
2022-02-11 11:26:06 -0800

    Update nodeForTree.cpp

--- nodeForTree.cpp
+++ nodeForTree.cpp
@@ -1,3 +1,4 @@
+//the node for the expression tree
 #include "nodeForTree.h"
 #include <iostream>
 #include <cstring>
@@ -11,35 +12,42 @@ nodeForTree::nodeForTree(){
   next = NULL;
 }
 
+//gets the left node
 nodeForTree* nodeForTree::getLeft(){
   return left;
 }
 
+//gets right node
 nodeForTree* nodeForTree::getRight(){
   return right;
 }
 
-
+//gets value stored in node
 char nodeForTree::getVal(){
   return val;
 }
 
+//sets the left
 void nodeForTree::setLeft(nodeForTree* newLeft){
   left = newLeft;
 }
 
+//sets the right
 void nodeForTree::setRight(nodeForTree* newRight){
   right = newRight;
 }
 
+//sets the value
 void nodeForTree::setVal(char newVal){
   val = newVal;
 }
 
+//gets the next node
 nodeForTree* nodeForTree::getNext(){
   return next;
 }
 
+//sets the next node
 void nodeForTree::setNext(nodeForTree* nextNew){
   next = nextNew;
 }
